090202d99afd32ceba5148058ebec12f3f134987
totorocupcake <cookcookcookie@hotmail.com>
2025-04-09 13:09:32 -0400

    delete migrations

--- priv/repo/migrations/20250325173331_rename_email_to_username.exs
+++
@@ -1,9 +0,0 @@
-defmodule Chatapp.Repo.Migrations.RenameEmailToUsername do
-  use Ecto.Migration
-
-  def change do
-    rename table(:users), :email, to: :username
-    drop_if_exists index(:users, [:email])
-    create unique_index(:users, [:username])
-  end
-end
